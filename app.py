ac7fff33822b596694b245b05f1d4eedb8bfe8f5
AlexAthayde <athayde_viana@hotmail.com>
2023-09-11 23:57:16 +0000

    Feat: Criadas variável e função para a base de dados e o import do sqlite

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask, render_template, request, session, abort, flash, redirect, url_for
 from posts import posts
+import sqlite3
 
 app = Flask("Meu App")
 app.config['SECRET_KEY'] = 'pudim'
+
+app.config.from_object(__name__)
+
+DATABASE = "banco.db"
+
+def conectar():
+    return sqlite3.connect(DATABASE)
 
 @app.route('/')
 def exibir_entradas():
